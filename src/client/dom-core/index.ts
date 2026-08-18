bfa8885f107f4fa2b32cb60118112b9e46da36ab
Emad Ghasemi <emadghasemi_2006@yahoo.com>
2026-08-18 08:18:15 +0330

    feat(dom): expose dom core public API (#4)

---
+++ src/client/dom-core/index.ts
@@ -0,0 +1,13 @@
+export { Fragment, h, isVNode, normalizeChildren, Slot } from "./vnode.ts";
+export type {
+  DomChild,
+  DomPrimitive,
+  ElementVNode,
+  FragmentVNode,
+  FunctionComponent,
+  SlotVNode,
+  VNode,
+  VNodeType,
+} from "./vnode.ts";
+export { isRenderableVNode, mountDom } from "./renderer.ts";
+export type { DomMount } from "./renderer.ts";
